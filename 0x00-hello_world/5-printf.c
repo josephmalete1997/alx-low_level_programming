a7fc1de85953ca48bd74343f074aa8cbec4c31f3
josephmalete1997 <josephmalete75@gmail.com>
2023-05-18 07:45:28 -0700

    5-printf.c fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-#include <string.h>
-
-int main(void){
-
-	
-	printf("with proper grammar, but the outcome is a piece of art \n");
-	return(0);
+/**
+*main - Main function
+*
+*
+*Return: return 0 everytime
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
