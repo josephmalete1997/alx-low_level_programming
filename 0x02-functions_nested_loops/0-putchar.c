9e383e36aeb92375b59da0214d95e87b526f1b27
josephmalete1997 <josephmalete75@gmail.com>
2023-05-25 08:06:57 +0000

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,9 +5,15 @@
 *
 *Return:return 0
 */
+
 int main(void)
 {
-	char put[10] = "_putchar";
-	_putchar(put);
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
+
 	return (0);
 }
