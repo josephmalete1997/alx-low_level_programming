9e295dbbd745e1b1eb08e9a45721233c0dfeab15
josephmalete1997 <josephmalete75@gmail.com>
2023-05-25 10:23:09 +0000

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower(int c) - checks if c is lowercase and return 1 else return 0
+ */
+
+int _islower(int c)
+{
+	if (_islower(c)){
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
