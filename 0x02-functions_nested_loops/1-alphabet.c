12179473575ab607d32d8bc00c988b91e01c96cb
josephmalete1997 <josephmalete75@gmail.com>
2023-05-25 08:59:24 +0000

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,9 +2,10 @@
 
 void  print_alphabet(void)
 {
-    char  alpha;
-    for (alpha='a';alpha<='z';++alpha){
-        _putchar(alpha);
+    char alpha;
+    for (alpha='a';alpha<='z';++alpha)
+    {
+    _putchar(alpha);
     }     
     _putchar('\n');
 }
