c9e6e04b73452462fe3d8d98fc9d1be4fbd057da
josephmalete1997 <josephmalete75@gmail.com>
2023-05-27 14:37:14 +0000

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- *print_most_numbers - prints 01356789
+ *print_most_numbers - prints numbers
+ * Created by Joseph jacob Malete
  *Return: void
  */
 void print_most_numbers(void)
