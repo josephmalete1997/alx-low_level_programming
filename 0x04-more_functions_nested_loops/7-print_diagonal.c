4ff5466f92718ee61ac31934631396eb6d3a1c99
josephmalete1997 <josephmalete75@gmail.com>
2023-05-28 23:24:53 +0000

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *print_diagonal - draw digonal lines on the terminal
+ *@n: number of times the character \ should be printed
+ *Return: void
+ */
+void print_diagonal(int n)
+{
+	int digonal,spacer;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (digonal = 1; digonal <= n; ++digonal)
+		{
+			for (spacer = 1; spacer <= n; ++spacer)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
