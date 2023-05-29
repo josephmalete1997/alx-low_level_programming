9c67d778b716d84013304ec1e2d23befbc898e19
josephmalete1997 <josephmalete75@gmail.com>
2023-05-29 00:48:51 +0000

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 /**
  *main - Entry point of our program
+ * Description: prints the numbers 1 - 100 w/
+ * Fizz for multiples of 3, Buzz for multiples of 5,
+ * and FizzBuzz for multiples of both
  *Return: 0
  */
 int main()
