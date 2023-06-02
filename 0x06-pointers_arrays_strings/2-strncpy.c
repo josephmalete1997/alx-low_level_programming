9eb9ab768b6ad44a41d991e2397d42544f411d93
josephmalete1997 <josephmalete75@gmail.com>
2023-06-02 02:22:26 +0000

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,9 +9,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int x;
 
-	for (x = 0; x < n; ++n)
-	{
+	for (x = 0; x < n && src[x] != '\0'; x++)
 		dest[x] = src[x];
+	while (x < n)
+	{
+		dest[x] = '\0';
+		x++;
 	}
-	return (0);
+
+	return (dest);
 }
